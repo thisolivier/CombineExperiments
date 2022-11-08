0fef26485ca6de9c8fd79ab989ef9fca0760211a
Olivier Butler <olivier@olivier.uk>
2022-11-08 20:34:58 +0000

    Initial Commit

---
+++ CombineTests/CombineTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTestsApp.swift
+//  CombineTests
+//
+//  Created by Olivier Butler on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
